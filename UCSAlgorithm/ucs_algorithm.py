160a86e60058afdd1f671bda7b0f875d22ea632e
TheSwordSlay <107758410+TheSwordSlay@users.noreply.github.com>
2023-03-20 18:51:26 +0700

    UCS added 1 example
    added 1 example for the test of UCS algorithm

--- UCSAlgorithm/ucs_algorithm.py
+++ UCSAlgorithm/ucs_algorithm.py
@@ -204,3 +204,31 @@ class UCS:
             if new_node.heuristic_value < old_node.heuristic_value:
               new_node.parent = selected_node
               self.insert_to_opened(new_node)
+
+def run():
+  graph = Graph()
+  graph.add_node(Node('V1'))
+  graph.add_node(Node('V2'))
+  graph.add_node(Node('V3'))
+  graph.add_node(Node('V4'))
+  graph.add_node(Node('V5'))
+  graph.add_node(Node('V6'))
+
+  graph.add_edge('V1', 'V2', 9)
+  graph.add_edge('V1', 'V3', 4)
+  graph.add_edge('V2', 'V3', 2)
+  graph.add_edge('V2', 'V4', 7)
+  graph.add_edge('V2', 'V5', 3)
+  graph.add_edge('V3', 'V4', 1)
+  graph.add_edge('V3', 'V5', 6)
+  graph.add_edge('V4', 'V5', 4)
+  graph.add_edge('V4', 'V6', 8)
+  graph.add_edge('V5', 'V6', 2)
+
+  alg = UCS(graph, "V1", "V6")
+  path, path_length = alg.search()
+  print(" -> ".join(path))
+  print(f"Lenghth of the path: {path_length}")
+
+if __name__ == '__main__':
+  run()
